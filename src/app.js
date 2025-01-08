0f3f53c7460e3055b859184d65031c7eca8d8aba
dibibabu <dibikannan@gmail.com>
2025-01-08 23:35:16 +0530

    data base connection added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 const express = require("express");
 
+const connectDB= require("./config/database")
+
 const app = express();
 
 app.use('/',(req,res)=>{
 
     res.send("hello world")
 })
+connectDB().then(()=>{
 
-app.listen(7777, () => {
-  console.log("app is listening to port 7777");
-});
+  console.log("database connected successfully");
+  app.listen(7777, () => {
+    console.log("app is listening to port 7777");
+  });
+  
+}).catch(()=>{
+  console.log("database connection error");
+  
+})
+
