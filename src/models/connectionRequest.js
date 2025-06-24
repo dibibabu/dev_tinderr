65a7c62b2c22f957bae8a88f632f5c256ed6a313
dibibabu <dibikannan@email.com>
2025-06-24 16:05:06 +0530

    added pre and indexes

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -22,5 +22,15 @@ const connectionRequestSchema = new mongoose.Schema(
 
   { timestamps: true }
 );
+connectionRequestSchema.index({fromUserId:1,toUserId:1})
+connectionRequestSchema.pre("save",function (next){
+  const connectionRequest=this
+if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+
+  throw new Error("cannot send request to yourself")
+  
+}
+next()
+})
 
 module.exports = mongoose.model("connectionRequest", connectionRequestSchema);
